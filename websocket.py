acc6cebc0a55c28d1e91a02985da2336ed28f2ea
秋水 <exp_data@126.com>
2018-06-22 22:35:27 +0800

    Create websocket.py

---
+++ websocket.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+#codz=yds
+import re
+import threading
+from websocket import create_connection
+
+def test_DNS_Servers():
+    global count
+    lock.acquire()
+    while True:
+        ws = create_connection("ws://123.207.167.163:9010/ajaxchattest")
+        ws.send("Hello, World>>1")##发送消息 eg:{'id':'1','un':'xx'}
+        result = ws.recv()##接收消息
+        count=count+1
+        #print (result)
+        #ws.close()
+        print (count)
+    lock.release()
+lock=threading.Lock()
+
+count=0
+threads = []
+for i in range(1000):
+    t = threading.Thread(target=test_DNS_Servers)
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+print (count)
+print ('All Done!')
